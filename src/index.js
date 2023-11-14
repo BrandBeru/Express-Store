a4f5317403b3bfb975f1fcc4e7c5e721211b0007
Giovanny Bernal <brand.beru@gmail.com>
2023-11-14 10:10:46 -0500

    fixed cors problems

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routerApi = require('./routes');
 const {checkApiKey} = require('./middlewares/auth.handler')
-//const cors = require('cors')
+const cors = require('cors')
 const { logErrors, errorHandler, boomErrorhandler, ormErrorHandler } = require('./middlewares/error.handler')
 
 const app = express();
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json())
 
-const whiteList = ['http://localhost:3000', 'https://branberu.tech']
+const whiteList = ['localhost:3000', 'https://api.branberu.tech']
 const options = {
   origin: (origin, callback) => {
     if(whiteList.includes(origin)){
@@ -19,9 +19,10 @@ const options = {
     callback(new Error('not allow'), false);
   }
 }
+app.use(cors(options))
 require('./utils/auth')
-app.get('/api/v1', checkApiKey, (req, res) => {
-  res.send('Hola mi server en express');
+app.get('/store/v1', checkApiKey, (req, res) => {
+  res.send('API para una tienda en linea con un server en express');
 })
 routerApi(app)
 app.use(logErrors)
